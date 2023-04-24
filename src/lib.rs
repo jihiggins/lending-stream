e59c55b7bedac181d2416b123275454321aa06f8
James Higgins <james.isaac.higgins@gmail.com>
2023-04-24 17:04:19 -0300

    Add feature impl_trait_in_assoc_type

--- src/lib.rs
+++ src/lib.rs
@@ -2,6 +2,7 @@
 #![feature(decl_macro)]
 #![feature(async_fn_in_trait)]
 #![feature(type_alias_impl_trait)]
+#![feature(impl_trait_in_assoc_type)]
 
 pub mod lending_stream;
 pub mod lending_stream_ext;
